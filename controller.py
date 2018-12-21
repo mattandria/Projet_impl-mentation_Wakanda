48c89b2024662d698ce084495ef4c94ddd1ab656
JohvanyROB <johvany.gustave@gmail.com>
2018-12-21 12:36:08 +0100

    model inverse python + controleur

---
+++ controller.py
@@ -0,0 +1,99 @@
+import AdaFruit_BBIO.PWM as PWM
+import Adafruit_BBIO.GPIO as GPIO
+import time
+from eqep import eQEP
+from threading import Thread
+
+class Controller(Thread):
+	#INITIALISATION
+	def__init__(self, periodPWM, periodEncoders, pinPWM1, pinDir1, pinPWM2, pinDir2):
+		Thread.__init__(self)
+		
+		#Encodeurs
+		self.encoder1 = eQEP("/sys/devices/ocp.3/48304000.epwmss/48304180.eqep", eQEP.MODE_ABSOLUTE)
+		self.encoder1.set_period(periodEncoders)
+		#self.encoder2 = eQEP("/sys/devices/ocp.3/48304000.epwmss/48304180.eqep", eQEP.MODE_ABSOLUTE)
+		#self.encoder2.set_period(periodEncoders)
+
+		#PWMs
+		self.pinPWM1 = pinPWM1
+		self.pinDir1 = pinDir1
+		self.pinPWM2 = pinPWM2
+		self.pinDir2 = pinDir2
+		self.PWM.start(pinDir1, 0, period)
+		self.PWM.start(pinDir2, 0, period)
+
+		#GPIO setup
+		self.GPIO.setup(pinDir1, GPIO.OUT)
+		self.GPIO.setup(pinDir2, GPIO.OUT)
+		
+		#PID
+		self.Kp = 10
+		self.Kd = 0
+		self.Ki = 0
+		self.lastError1 = 0
+		self.totalError1 = 0
+		self.lastError2 = 0
+		self.totalError2 = 0
+
+	#SEND COMMAND TO A MOTOR
+	def sendCmd(self, motor, val):
+		if(motor == 1):
+			self.PWM.set_duty_cycle(pinPWM1, val)
+		else:
+			self.PWM.set_duty_cycle(pinDir2, val)
+
+	#CALCULATE THE COMMAND TO SEND TO EACH MOTOR
+	def execute(self):
+		#PID FOR MOTOR1
+		error1 = self.des1 - self.encoder1.poll_position()/4
+		pTerm1 = self.Kp*error1
+		dTerm1 = self.Kd*(error1 - self.lastError)
+		self.totalError1+= error1
+		iTerm1 = self.Ki*self.totalError1
+		lastError1 = error1
+		cmd1 = pTerm1 + dTerm1 + iTerm1
+
+		sendCmd(1, regulateCmd(1, cmd1))
+
+
+		#PID FOR MOTOR2
+		error2 = self.des2 - self.encoder2.poll_position()/4
+		pTerm2 = self.Kp*error2
+		dTerm2 = self.Kd*(error2 - self.lastError)
+		self.totalError2+= error2
+		iTerm2 = self.Ki*self.totalError2
+		lastError2 = error2
+		cmd2 = pTerm2 + dTerm2 + iTerm2
+
+		sendCmd(2, regulateCmd(2, cmd2))
+
+
+	#STOP BOTH MOTORS
+	def stop(self):
+		sendCmd(1, 0)
+		sendCmd(2, 0)
+
+	#SET A NEW DESIRED MOTOR ORIENTATION
+	def setDes(self, val1, val2):
+		self.des1 = val1
+		self.des2 = val2
+
+	#REGULATE THE COMMAND AND THE MOTOR DIRECTION
+	def regulateCmd(self, motor, cmd):
+		pinDir = pinDir1
+		if(motor == 2):
+			pinDir = pinDir2
+
+		if(cmd > 0):
+			self.GPIO.output(pinDir, GPIO.HIGH)
+			if(cmd > 100)	#le duty_cycle ne peut pas depasser la valeur de la periode
+				cmd = 100
+		elif(cmd < 0):
+			self.GPIO.output(pinDir, GPIO.LOW)
+			cmd *= -1	#le duty cycle doit etre positif
+			if(cmd > 100)
+				cmd = 100
+		return cmd
+
+
---
+++ wakanda.py
@@ -0,0 +1,43 @@
+#from controller import Controller
+from math import sqrt, acos, cos, atan2, pow
+
+class Wakanda(object):
+	#initialisation
+	def __init__(self, periodPWM, periodEncoders):
+		#self.controller = Controller(1000000, 100000000, "P8_13", "P8_8", "P8_19", "P8_17")
+		self.og = [-10,0]
+		self.od = [10,0]
+		self.l1 = 60
+		self.l2 = 80
+
+	#DETERMINE THE DESIRED ORIENTATION OF THE MOTORS WRT A DESIRED POSE
+	def modeleInverse(self, x, y):
+		dg = sqrt( pow( x - self.og[0], 2) + pow(y, 2) )
+		dd = sqrt( pow(x - self.od[0], 2) + pow(y,2) )
+		thetad = atan2( y/dd, (x - self.od[0])/dd )
+		thetag = atan2( y/dg, (x - self.og[0])/dg )
+
+
+		d1 = pow(x - self.od[0], 2) + pow(y, 2) - pow(self.l1, 2) - pow(self.l2, 2)
+		d2 = pow(x - self.og[0], 2) + pow(y, 2) - pow(self.l1, 2) - pow(self.l2, 2)
+
+		print d1
+		print d2
+		
+		if abs(d1) < 2*self.l1*self.l2 and abs(d2) < 2*self.l1*self.l2 :
+			betad = acos( d1/(2*self.l1*self.l2) )
+			betag = acos( d2/(2*self.l1*self.l2) )
+			phid = acos( (self.l1 + self.l2*cos(betad)) / dd )
+			phig = acos( (self.l1 + self.l2*cos(betag)) / dg )
+			theta1 = thetag + phig
+			theta2 = thetad - phid
+
+			print "theta1 = {}, theta2 = {}".format(theta1, theta2)
+
+		return [theta1, theta2]
+
+
+wakanda = Wakanda(10, 10)
+x = 20
+y = 30
+wakanda.modeleInverse(x, y)
