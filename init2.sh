3c141299cdabc2a1b92a244c3bd046eed16ce420
JohvanyROB <johvany.gustave@gmail.com>
2019-01-08 11:19:47 +0100

    modif init

--- init2.sh
+++ init2.sh
@@ -2,20 +2,18 @@ export SLOTS=/sys/devices/bone_capemgr.9/slots
 echo cape-universaln > $SLOTS
 
 #####Activate PWM######
-#On met le Pin 8_13 en mode PWM
-cd /sys/devices/ocp.3/P8_13_pinmux.21/
-echo pwm > state  
-
-cd .. && cd 48304000.epwmss/48304200.ehrpwm/pwm/pwmchip5/
+export OCP3=/sys/devices/ocp.3
+echo pwm > $OCP3/P8_13_pinmux.21/state  #On met le Pin 8_13 en mode PWM
+echo pwm > $OCP3/P8_19_pinmux.27/state  #On met le Pin 8_13 en mode PWM
 
 #On fait correspondre le Pin au bon module PWM (grâce à son numéro)
-cd /sys/class/pwm/
-echo 5 > export
-echo 6 > export
+export PWM=/sys/class/pwm/
+echo 5 > $PWM/export #p8_19
+echo 6 > $PWM/export #p8_13
 
+echo 1 > $PWM/pwm5/run
+echo 1 > $PWM/pwm6/run
 
 #Activate P8_11 && P8_12 pins for the encoder
-cd /sys/devices/ocp.3/P8_11_pinmux.19/
-echo qep > state
-cd /sys/devices/ocp.3/P8_12_pinmux.20/
-echo qep > state
+echo qep > $OCP3/P8_11_pinmux.19/state
+echo qep > $OCP3/P8_12_pinmux.20/state
