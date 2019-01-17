f465cfd22d6cdea16fbdd2fbd71a69ae3030172e
JohvanyROB <johvany.gustave@gmail.com>
2019-01-17 22:18:48 +0100

    documentation pwm + eqep

--- eqep.py
+++ eqep.py
@@ -1,122 +1,94 @@
-# API for the TI eQEP hardware driver I wrote
-
 # We need OS operations for this
 import os, select
 
+## \brief Classe permettant de gerer la position du moteur
 class eQEP(object):
     # Modes
     MODE_ABSOLUTE = 0
     MODE_RELATIVE = 1
 
-    # Set the mode of the eQEP hardware
+    ## \brief Set the mode of the eQEP hardware
+    # @param mode The mode of the eQEP hardware
     def set_mode(self, mode):
-        # Open the mode attribute file
-        attribute = open(self.path + "/mode", "w")
-        
-        # Write the desired mode into the file
-        attribute.write(str(mode))
-        
-        # Close the file
-        attribute.close()
-        
-    # Get the mode of the eQEP hardware
+        attribute = open(self.path + "/mode", "w")  # Open the mode attribute file
+
+        attribute.write(str(mode))  # Write the desired mode into the file
+
+        attribute.close()   # Close the file
+
+    ## \brief Get the mode of the eQEP hardware
     def get_mode(self):
-        # Open the attribute file
-        attribute = open(self.path + "/mode", "r")
-        
-        # Get the value
-        mode = int(attribute.readline())
-        
-        # Close the attribute
-        attribute.close()
-        
-        # Return the mode
-        return mode
+        attribute = open(self.path + "/mode", "r")  # Open the attribute file
 
-    # Set the unit timer period of the eQEP hardware
+        mode = int(attribute.readline())    # Get the value
+
+        attribute.close()   # Close the attribute
+
+        return mode # Return the mode
+
+    ## \brief Set the unit timer period of the eQEP hardware
+    # @param period The unit timer period of the eQEP hardware
     def set_period(self, period):
-        # Open the mode attribute file
-        attribute = open(self.path + "/period", "w")
-        
-        # Write the desired mode into the file
-        attribute.write(str(period))
-        
-        # Close the file
-        attribute.close()
-        
-    # Get the unit timer period of the eQEP hardware
-    def get_period(self):
-        # Open the attribute file
-        attribute = open(self.path + "/period", "r")
-        
-        # Get the value
-        period = int(attribute.readline())
-        
-        # Close the attribute
-        attribute.close()
-        
-        # Return the mode
-        return period
-        
-    # Set the current position of the encoder hardware
-    def set_position(self, position):
-        # Open the mode attribute file
-        attribute = open(self.path + "/position", "w")
-        
-        # Write the desired mode into the file
-        attribute.write(str(position))
-        
-        # Close the file
-        attribute.close()
-        
-    # Get the immediate position of the encoder hardare
-    def get_position(self):
-        # Open the attribute file
-        attribute = open(self.path + "/position", "r")
-        
-        # Get the value
-        position = int(attribute.readline())
-        
-        # Close the attribute
-        attribute.close()
-        
-        # Return the mode
-        return position
-        
-    # Poll the position, returns when new data is available
-    def poll_position(self):
-        # Poll the position file
-        self.poller.poll(-1)
-        
-        # Seek to the beginning of the file to get the data
-        os.lseek(self.fd, 0, 0)
-        
-        # Return the position
-        return int(os.read(self.fd, 16))
-    
-    # Constructor - specify the path and the mode
-    def __init__(self, path, mode):
-        # Base path of the eQEP sysfs entry (ex. /sys/devices/ocp.2/48302000.epwmss/48302180.eqep)
-        self.path = path;
-        
-        # Set the mode
-        self.set_mode(mode)
+        attribute = open(self.path + "/period", "w")    # Open the mode attribute file
 
-        # Reset the position
-        self.set_position(0)
-        
-        # Setup polling system
-        self.fd = os.open(self.path + "/position", os.O_RDONLY, os.O_NONBLOCK)
-       
-        # Create the poll object
-        self.poller = select.poll()
-        self.poller.register(self.fd, select.POLLPRI)  
-        
-    # Deconstructor
+        attribute.write(str(period))    # Write the desired mode into the file
+
+        attribute.close()   # Close the file
+
+    ## \brief Get the unit timer period of the eQEP hardware
+    def get_period(self):
+        attribute = open(self.path + "/period", "r")    # Open the attribute file
+
+        period = int(attribute.readline())  # Get the value
+
+        attribute.close()   # Close the attribute
+
+        return period   # Return the mode
+
+    ## \brief Set the current position of the encoder hardware
+    # @param position The new position of the encoder hardware
+    def set_position(self, position):
+        attribute = open(self.path + "/position", "w")  # Open the mode attribute file
+
+        attribute.write(str(position))  # Write the desired mode into the file
+
+        attribute.close()   # Close the file
+
+    ## \brief Get the immediate position of the encoder hardware
+    def get_position(self):
+        attribute = open(self.path + "/position", "r")  # Open the attribute file
+
+        position = int(attribute.readline())    # Get the value
+
+        attribute.close()   # Close the attribute
+
+        return position # Return the mode
+
+    ## \brief Poll the position, returns when new data is available
+    def poll_position(self):
+        self.poller.poll(-1)    # Poll the position file
+
+        os.lseek(self.fd, 0, 0) # Seek to the beginning of the file to get the data
+
+        return int(os.read(self.fd, 16))    # Return the position
+
+    ## \brief Constructor - specify the path and the mode
+    # @param path The path of the encoder
+    # @param mode The desired mode: 0 (MODE_ABSOLUTE), 1 (MODE_RELATIVE)
+    def __init__(self, path, mode):
+        self.path = path;   # Base path of the eQEP sysfs entry
+
+        self.set_mode(mode) # Set the mode
+
+        self.set_position(0)    # Reset the position
+
+        self.fd = os.open(self.path + "/position", os.O_RDONLY, os.O_NONBLOCK)  # Setup polling system
+
+        self.poller = select.poll() # Create the poll object
+        self.poller.register(self.fd, select.POLLPRI)
+
+    ## \brief Deconstructor
     def __del__(self):
         # Cleanup polling system
         self.poller.unregister(self.fd)
-        os.close(self.fd)   
-        
-    
-
+        os.close(self.fd)
--- pwm.py
+++ pwm.py
@@ -1,55 +1,53 @@
-# API for the TI eQEP hardware driver I wrote
+import os # We need OS operations for this
 
-# We need OS operations for this
-import os
-import Adafruit_BBIO.GPIO as GPIO
-
+## \brief Classe permettant de gerer le module PWM
 class PWM(object):
-    # Set the unit timer period of the PWM hardware
+
+    ## \brief Definit le duty_cycle
+    # @param pinPWM Le pin de PWM
+    # @param duty_cycle La valeur du duty_cycle souhaite
     def set_duty_cycle(self, pinPWM, duty_cycle):
+        #On ecrit dans le bon dossier en fonction du pin de PWM
         pwm = ""
         if(pinPWM == "P8_19"):
-            pwm = "pwm5"
+            pwm = "pwm0"
         else:
-            pwm = "pwm6"
+            pwm = "pwm1"
 
-        # Open the mode attribute file
-        attribute = open(self.path + "/" + pwm + "/duty_ns", "w")
+        attribute = open(self.path + "/" + pwm + "/duty_cycle", "w")    #Ouvre le fichier
 
-        duty_cycle = duty_cycle*self.period/100
-	print "duty_cycle: {}".format(duty_cycle)
+        duty_cycle = duty_cycle*self.period/100 #On convertit le duty_cycle en pourcentage
+        print "duty_cycle: {}".format(duty_cycle)
 
-        # Write the desired mode into the file
-        attribute.write(str(duty_cycle))
+        attribute.write(str(duty_cycle))    #Ecrit la valeur desiree dans le fichier
 
-        # Close the file
-        attribute.close()
+        attribute.close()   #On ferme le fichier
 
-    # Set the unit timer period of the PWM hardware
+    ## \brief Definit la periode du PWM
+    # @param pinPWM Le pin de PWM
+    # @param period La valeur de la periode souhaitee
     def set_period(self, pinPWM, period):
+        #On ecrit dans le bon dossier en fonction du pin de PWM
         pwm = ""
         if(pinPWM == "P8_19"):
-            pwm = "pwm5"
+            pwm = "pwm0"
         else:
-            pwm = "pwm6"
+            pwm = "pwm1"
 
-        # Open the mode attribute file
-        attribute = open(self.path + "/" + pwm + "/period_ns", "w")
-        
+        attribute = open(self.path + "/" + pwm + "/period", "w")    #Ouvre le fichier
+
         self.period = period
 
-        # Write the desired mode into the file
-        attribute.write(str(self.period))
+        attribute.write(str(self.period))   # Ecrit la valeur desiree dans le fichier
 
-        # Close the file
-        attribute.close()
+        attribute.close()   #ferme le fichier
 
-    # Constructor - specify the path and the mode
+    ## \brief Constructor
     def __init__(self):
-        self.path = "/sys/class/pwm"
+        self.path = "/sys/class/pwm/pwmchip6"    #Chemin qui permet d'acceder aux pwm utilises
         self.period = 0
-        print "created"
 
+    ## \brief Envoie un duty_cycle de 0
+    # @param pinPWM Le pin de PWM
     def stop(self, pinPWM):
         self.set_duty_cycle(pinPWM, 0)
-
